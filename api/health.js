05e28565f16c6a39b2a177693c217018bed9ce4c
Prestes16 <cleitonprestes54@gmail.com>
2025-09-24 17:50:24 -0300

    chore(health): usar sendJson para incluir header X-App-Version (0.3.5)

--- api/health.js
+++ api/health.js
@@ -1,8 +1,9 @@
 // api/health.js
 import { APP_VERSION } from './_version.js';
+import { sendJson } from './_utils.js';
 
 export default async function handler(req, res) {
-  res.status(200).json({
+  return sendJson(res, 200, {
     ok: true,
     service: 'bags-shield-api',
     version: APP_VERSION,
@@ -10,3 +11,4 @@ export default async function handler(req, res) {
     network: 'devnet'
   });
 }
+
