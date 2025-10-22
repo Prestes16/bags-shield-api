ad680bcdb399cdeea806cfa97d9f12e761417588
Prestes16 <cleitonprestes54@gmail.com>
2025-10-22 15:18:36 -0300

    fix(esm): add .js extension for ESM import to lib/bags

--- api/bags/token-info/index.ts
+++ api/bags/token-info/index.ts
@@ -1,6 +1,6 @@
 export const config = { runtime: "nodejs" };
 
-import { getTokenInfo } from "../../../lib/bags";
+import { getTokenInfo } from "../../../lib/bags.js";
 
 function setBaseHeaders(res: any, requestId: string) {
   res.setHeader("Access-Control-Allow-Origin", "*");
